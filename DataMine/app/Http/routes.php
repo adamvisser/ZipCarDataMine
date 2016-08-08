3961365d515f0bc23a668d0b453e1cffdf4338d3
Adam Visser <adamvissers@gmail.com>
2016-08-08 03:12:21 -0700

    checking all is updated before using my usual login stuffs

--- DataMine/app/Http/routes.php
+++ DataMine/app/Http/routes.php
@@ -15,11 +15,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//do you even comment adam, as this line is important
 Route::post('/submitdata', 'DataSubmissionController@submitData');
 
 
 //we will want to know which building is being traveled to the most, and from which building
 Route::get('/api/graphs/destination/treemap/{startTurn?}/{endTurn?}','DataSubmissionController@checkPeople');
-
 //we will want to know which building is being waited at the longest, and for what building
 Route::get('/api/graphs/origin/treemap/{startTurn?}/{endTurn?}','DataSubmissionController@checkPeople');
+
+
+Route::match(['get', 'post'],
+	'/login',
+	'MainController@login');
